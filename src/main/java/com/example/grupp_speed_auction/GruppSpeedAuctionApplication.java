09c8643cef708ef3c4a55af78645893e270b2476
abocis <90641949+abocis@users.noreply.github.com>
2024-01-19 09:37:31 +0100

    first

---
+++ src/main/java/com/example/grupp_speed_auction/GruppSpeedAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupp_speed_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppSpeedAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppSpeedAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupp_speed_auction/GruppSpeedAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupp_speed_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppSpeedAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
