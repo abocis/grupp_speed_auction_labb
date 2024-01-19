7f3f8f305ed513bb5126239110a339e65d2bfa3b
abocis <90641949+abocis@users.noreply.github.com>
2024-01-19 11:56:02 +0100

    cotrollerfixat

--- src/main/java/com/example/grupp_speed_auction/Controllers/RecipeController.java
+++ src/main/java/com/example/grupp_speed_auction/Controllers/RecipeController.java
@@ -1,2 +1,27 @@
-package com.example.grupp_speed_auction.Controllers;public class RecipeController {
+package com.example.grupp_speed_auction.Controllers;
+
+import com.example.grupp_speed_auction.models.RecipeModel;
+import com.example.grupp_speed_auction.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+
+public class RecipeController {
+    @Autowired
+    RecipeService recipeService;
+
+    //POST
+    @PostMapping("/recipe")
+    public RecipeModel createRecipeModel(RecipeModel recipeModel){
+
+        return recipeService.createRecipeModel(recipeModel);
+    }
+
+
+
 }
