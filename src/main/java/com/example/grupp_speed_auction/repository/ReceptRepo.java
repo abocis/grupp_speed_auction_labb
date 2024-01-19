6772c1883b0280c72bd01c6995afdc98b04e3a28
andronisdre <andrebulow13@gmail.com>
2024-01-19 11:17:22 +0100

    added everything to the ReceptRepo interface

--- src/main/java/com/example/grupp_speed_auction/repository/ReceptRepo.java
+++ src/main/java/com/example/grupp_speed_auction/repository/ReceptRepo.java
@@ -1,2 +1,7 @@
-package com.example.grupp_speed_auction.repository;public interface ReceptRepo {
+package com.example.grupp_speed_auction.repository;
+
+import com.example.grupp_speed_auction.models.RecipeModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReceptRepo extends MongoRepository<RecipeModel, String> {
 }
