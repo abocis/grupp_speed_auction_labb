f53e575dd5f7e4974bf2b8cf11410d97c56ba51a
andronisdre <andrebulow13@gmail.com>
2024-01-19 11:27:55 +0100

    fixed RecipeService in service

--- src/main/java/com/example/grupp_speed_auction/service/RecipeService.java
+++ src/main/java/com/example/grupp_speed_auction/service/RecipeService.java
@@ -1,2 +1,16 @@
-package com.example.grupp_speed_auction.service;public class RecipeService {
+package com.example.grupp_speed_auction.service;
+
+import com.example.grupp_speed_auction.models.RecipeModel;
+import com.example.grupp_speed_auction.repository.ReceptRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeService {
+    @Autowired
+    ReceptRepo receptRepo;
+
+    public RecipeModel createRecipeModel(RecipeModel recipeModel) {
+        return receptRepo.save(recipeModel);
+    }
 }
